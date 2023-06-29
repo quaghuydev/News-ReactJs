445023d016f02c6bc85ba779700b5b696aae6a18
BuiQuangHuy <20130276@st.hcmuaf.edu.vn>
2023-06-29 16:06:35 +0700

    upload project build a News app by reactjs

--- src/components/pages/DetailNews/DetailNews.js
+++ src/components/pages/DetailNews/DetailNews.js
@@ -1,48 +1,63 @@
-import style from './DetailNews.module.scss'
 import useGetDetailNews from "../../../data/useGetDetailNews";
-import useGetNewsAPI from "../../../data/useGetNewsAPI";
-import {useLoaderData, useNavigate, useParams} from "react-router-dom";
-import React from "react";
-
-async function getNews(url) {
-    const link = await useGetNewsAPI(url)
-    const data = await  useGetDetailNews(link)
-    return {data}
-}
+import {useLoaderData, useNavigate} from "react-router-dom";
+import Comment from "./Component/Comment";
+import React, {useMemo} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faClock, faUser} from "@fortawesome/free-solid-svg-icons";
+import style from './Detail.module.scss';
+import Content from "./Component/Content";
+import TextToSpeech from "./Component/TextToSpeech";
 
 export const loadNewsDetail = async ({params}) => {
-    // const dataNews =params.news
-    const {link} =params
-    const Url="/api/"+link
-    console.log(Url)
+    const {link} = params
+    const Url = "/api/" + link
     return Url;
 }
 
 function DetailNews() {
-    // const url = useGetNewsAPI("https://danviet.vn/hlv-park-hang-seo-ra-di-de-lai-bo-mat-tro-trui-cua-u22-viet-nam-20230501070522595.htm")
-    // const {data} = getNews(url)
-    // console.log( getNews("https://danviet.vn/hlv-park-hang-seo-ra-di-de-lai-bo-mat-tro-trui-cua-u22-viet-nam-20230501070522595.htm"))
-    const Url = useLoaderData()
-    const data = useGetDetailNews(Url)
+    const Url = useLoaderData();
+    const memoizedUrl = useMemo(() => Url, []);
+    const data = useGetDetailNews(memoizedUrl);
     const back = useNavigate();
+    const newUrl = Url.replace("/api/", "");
     return (
-        <div className={style['wrapper']}>
-            <div className={style['dt-relate']}>tin liên quan</div>
-            <div className={style['dt-main']}>
-                <button className={style['btn-back']} onClick={()=>back((-1))}>Trở về</button>
+        <div className={style["wrapper"]}>
+            <div className={style["wrapmain"]}>
+                <div className={style["main-contain"]}>
+                    <div className={style["detail-main"]}>
+                        <div className={style["entry-body"]}>
+                            {data ?
+                                (
+                                    <>
+                                        <h2 className={style["title"]}>{(data.title)}</h2>
+                                        <div className={style["line-datetime"]}>
+                                            <div>
+                                                <span><FontAwesomeIcon icon={faUser}/>{data.author}</span>
+                                                <span data-role={style["publishdate"]}><FontAwesomeIcon
+                                                    icon={faClock}/>{data.dateTime}</span>
+                                            </div>
+                                            <div className={style["btn-sizep"]}>
+                                                <span className={style["size-default"]}>Aa</span>
+                                                <span className={style["size-plus"]}>Aa+</span>
+                                            </div>
+                                        </div>
+                                        <div className={style["sapo"]}>{data.sapo}</div>
+                                        <Content content={data.content}/>
+                                    </>
+                                ) : (<p>Loading...</p>)
+                            }
+                        </div>
+                    </div>
 
-                {data ? (
-                    <>
-                        <h3 className={style['title']}>{(data.title)}</h3>
-                        <p content={style['content']}>{data.content.content1}</p>
-                        <img className={style['image']} src={data.images[1]} alt=""/>
-                        <p content={style['content']}>{data.content.content2}</p>
-                        <img className={style['image']} src={data.images[2]} alt=""/>
-                        <p content={style['content']}>{data.content.content3}</p>
-                    </>
-                ) : (
-                    <p>Loading...</p>
-                )}
+                </div>
+                <div className={style["sidebar"]}>
+                    <div className={style["text-to-speech"]}>
+                        <button className={style['btn-back']} onClick={() => back((-1))}>Trở về</button>
+                        {data ? <TextToSpeech text={data.text}/> : (<p>Loading...</p>)}</div>
+                    <div className={style["comment-fb"]}>
+                        <Comment data={newUrl} />
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/data/useGetDetailNews.js
+++ src/data/useGetDetailNews.js
@@ -1,22 +1,6 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import axios from 'axios';
 import cheerio from 'cheerio';
-
-function splitTextIntoChunks(result, numChunks = 3) {
-    const words = result.split(' ');
-    const chunkSize = Math.ceil(words.length / numChunks);
-    const chunks = [];
-    for (let i = 0; i < words.length; i += chunkSize) {
-        const chunk = words.slice(i, i + chunkSize).join(' ');
-        chunks.push(chunk);
-    }
-    const obj = {
-        content1: chunks[0],
-        content2: chunks[1],
-        content3: chunks[2]
-    };
-    return obj;
-}
 
 function useGetDetailNews(url) {
     const [newsDetail, setNewsDetail] = useState(null);
@@ -27,29 +11,26 @@ function useGetDetailNews(url) {
                 const html = response.data;
                 const $ = cheerio.load(html);
 
-                const newsTitle = $('span.title').first().text().replace(/\s+/g, ' ').trim();
+                $('div.detail-main div.dt-content div.entry-body div.kbwscwl-relatedbox').remove();
 
-                let content = $('.dt-content').text().replace(/\s+/g, ' ').trim();
-                const sentences = content.split(/[.?!]/);
-                const filteredSentences = sentences.filter(sentence => !sentence.includes('Ảnh: '));
-                content = filteredSentences.join('. ');
+                const entryBody = $("div.detail-main div.dt-content div.entry-body");
+                const contents = entryBody.contents();
+                const range = document.createRange();
+                const entryBodyFragment = range.createContextualFragment(contents);
 
-                const image = $('meta[property="og:image"]').attr('content');
-
+                const title = $("div.detail-main .dt-title h2 span.title").text();
+                const sapo = $("div.detail-main div.sapo").text();
+                const textBody = $("div.detail-main div.dt-content div.entry-body").text();
+                const author = $("div.detail-main div.line-datetime span.anots").text();
+                const dateTime = $("div.detail-main div.line-datetime span[data-role='publishdate']").text();
                 const result = {
-                    title: newsTitle,
-                    content: splitTextIntoChunks(content, 3),
-                    image: image
+                    title:title,
+                    sapo:sapo,
+                    content: entryBodyFragment,
+                    author: author?author:'Anonymous',
+                    dateTime:dateTime,
+                    text:title+sapo+textBody,
                 };
-
-                const images = [];
-                $('.dt-content').find('img').each((index, element) => {
-                    const src = $(element).attr('src');
-                    images.push(src);
-                });
-
-                result.images = images.slice(0, 4);
-
                 setNewsDetail(result);
             })
             .catch(error => {
@@ -61,62 +42,3 @@ function useGetDetailNews(url) {
 }
 
 export default useGetDetailNews;
-// import { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import cheerio from 'cheerio';
-//
-// function useGetDetailNews(url) {
-//     const [newsDetail, setNewsDetail] = useState(null);
-//
-//     useEffect(() => {
-//         async function fetchData() {
-//             try {
-//                 const response = await axios.get(url);
-//                 const html = response.data;
-//                 const $ = cheerio.load(html);
-//                 const newsTitle = $('span.title').first().text().replace(/\s+/g, ' ').trim();
-//                 let content = $('.dt-content').text().replace(/\s+/g, ' ').trim();
-//                 const sentences = content.split(/[.?!]/);
-//                 const filteredSentences = sentences.filter(sentence => !sentence.includes('Ảnh: '));
-//                 content = filteredSentences.join('. ');
-//                 const image = $('meta[property="og:image"]').attr('content');
-//                 const result = {
-//                     title: newsTitle ,
-//                     content: splitTextIntoChunks(content, 3),
-//                     image: image
-//                 };
-//                 const images = [];
-//                 $('.dt-content').find('img').each((index, element) => {
-//                     const src = $(element).attr('src');
-//                     images.push(src);
-//                 });
-//                 const limitedImages = images.slice(0, 4);
-//                 result.images = limitedImages;
-//                 setNewsDetail(result);
-//             } catch (error) {
-//                 console.error(error);
-//             }
-//         }
-//         fetchData();
-//     }, [url]);
-//
-//     function splitTextIntoChunks(result, numChunks = 3) {
-//         const words = result.split(' ');
-//         const chunkSize = Math.ceil(words.length / numChunks);
-//         const chunks = [];
-//         for (let i = 0; i < words.length; i += chunkSize) {
-//             const chunk = words.slice(i, i + chunkSize).join(' ');
-//             chunks.push(chunk);
-//         }
-//         const obj = {
-//             content1: chunks[0],
-//             content2: chunks[1],
-//             content3: chunks[2]
-//         };
-//         return obj;
-//     }
-//
-//     return newsDetail;
-// }
-//
-// export default useGetDetailNews;
